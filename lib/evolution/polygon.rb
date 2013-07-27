64b290ee77703964392e04f2e9bd99dc9e08f45d
Joey Schoblaska <joey.schoblaska@gmail.com>
2013-07-27 08:03:53 -0500

    simplified mutation calculation

--- lib/evolution/polygon.rb
+++ lib/evolution/polygon.rb
@@ -4,10 +4,7 @@ module Evolution
 
     def self.calculate_mutation(var = {})
       min, max, initial = var[:min] || 0, var[:max] || 255, var[:initial]
-
-      upwards = (((rand + 1) ** 12.0 / 4096) * (max - initial)).to_i
-      downwards = (((rand + 1) ** 12.0 / 4096) * (initial - min)).to_i
-      return (initial + upwards - downwards).restrict(:min => min, :max => max)
+      (initial + rand(5) - rand(5)).restrict(:min => min, :max => max)
     end
 
     def initialize(other = nil)
